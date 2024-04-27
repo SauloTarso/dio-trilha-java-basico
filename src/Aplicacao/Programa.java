3f24644f3291ed4445652126606e33b1e2e5fae7
SauloTarso <saulotlamorim@gmail.com>
2024-04-27 17:57:24 -0300

    Criando a classe Programa no pacote Aplicacao

--- src/Aplicacao/Programa.java
+++ src/Aplicacao/Programa.java
@@ -1,8 +1,30 @@
 package Aplicacao;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import entidades.ContaTerminal;
+
 public class Programa {
 
 	public static void main(String[] args) {
-		
+		Locale.setDefault(Locale.US);
+		Scanner ler = new Scanner(System.in);
+
+		System.out.print("Digite o número da conta: ");
+		int numeroConta = ler.nextInt();
+		System.out.print("Digite o número da agência: ");
+		String numeroAgencia = ler.next();
+		System.out.print("Nome completo do titular: ");
+		ler.nextLine();
+		String nomeCliente = ler.nextLine();
+		System.out.print("Digite o saldo: ");
+		double saldo = ler.nextDouble();
+
+		ContaTerminal contaTerminal = new ContaTerminal(numeroConta, numeroAgencia, nomeCliente, saldo);
+
+		System.out.println(contaTerminal);
+
+		ler.close();
 	}
 }
